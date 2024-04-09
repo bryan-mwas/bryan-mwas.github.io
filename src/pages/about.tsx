47d6eab49d89d00717b9142bfb08c37a4ddad206
BM <mwathibrian7@gmail.com>
2024-04-09 05:25:51 +0300

    UI: Update text alignment

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -3,11 +3,11 @@ import { HeadFC } from "gatsby";
 
 function About() {
   return (
-    <article className="text-2xl  font-medium leading-relaxed text-gray-900 dark:text-white">
+    <article className="text-lg font-medium leading-relaxed text-gray-900 dark:text-white">
       <p className="mb-2 text-center">
         Hello <span className="text-3xl">👋🏾</span>
       </p>
-      <p className="text-justify">
+      <p className="mb-2">
         I’m a software engineer with 5 years of experience, passionate about the
         web and databases. Throughout my career, I have designed and built web
         applications in React, TypeScript, MongoDB, and NestJS Framework that
@@ -15,7 +15,7 @@ function About() {
         problem-solver who has worked with cross-functional teams to deliver
         business solutions.
       </p>
-      <p>More</p>
+      <p className="text-slate-600">More to come...</p>
     </article>
   );
 }
--- src/pages/posts.tsx
+++ src/pages/posts.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 function Posts() {
-  return <p className="text-center text-2xl text-slate-700">Coming soon</p>;
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <p className="text-center text-2xl text-slate-700">
+        This page is under development.
+      </p>
+    </div>
+  );
 }
 
 export default Posts;
