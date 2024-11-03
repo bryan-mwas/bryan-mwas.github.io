b65d182299e4585f894d71ecb71e5bad25cdb508
Mwathi <mwathibrian7@gmail.com>
2024-11-03 22:38:38 +0300

    Added Google Analytics

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -14,7 +14,7 @@ const config: GatsbyConfig = {
     {
       resolve: "gatsby-plugin-google-gtag",
       options: {
-        trackingIds: ["GA-TRACKING_ID"],
+        trackingIds: ["G-K3HXYVPTVN"],
         // This object gets passed directly to the gtag config command
         // This config will be shared across all trackingIds
         gtagConfig: {
