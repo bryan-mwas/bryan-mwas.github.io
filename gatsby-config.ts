c927c1feef48d78c1cb946f346e44bd3c3834012
Mwathi <mwathibrian7@gmail.com>
2024-11-04 06:10:41 +0300

    Bugfix: Added origin to google analytics

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -14,7 +14,7 @@ const config: GatsbyConfig = {
     {
       resolve: "gatsby-plugin-google-gtag",
       options: {
-        trackingIds: ["G-K3HXYVPTVN"],
+        trackingIds: ["G-K3HXYVPTVN", "GT-P85BCD2Z"],
         // This object gets passed directly to the gtag config command
         // This config will be shared across all trackingIds
         gtagConfig: {
@@ -31,7 +31,7 @@ const config: GatsbyConfig = {
           // Avoids sending pageview hits from custom paths
           exclude: ["/preview/**", "/do-not-track/me/too/"],
           // Defaults to https://www.googletagmanager.com
-          origin: "YOUR_SELF_HOSTED_ORIGIN",
+          origin: "https://www.googletagmanager.com",
           // Delays processing pageview events on route update (in milliseconds)
           delayOnRouteUpdate: 0,
         },
